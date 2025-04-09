a37d394d43f644116b05aae0279fe27af49a1835
Yuliya Kalinina <u.v.zaika@gmail.com>
2025-04-09 09:25:16 +0400

    preparation for deploymen

--- next.config.ts
+++ next.config.ts
@@ -1,12 +1,13 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  output: 'export',
-  basePath: '/cardsOfProducts',
+  output: "export",
+  basePath: process.env.NODE_ENV === "production" ? "/cardsOfProducts" : "",
+  assetPrefix: process.env.NODE_ENV === "production" ? "/cardsOfProducts/" : "",
   images: {
     domains: ["cdn2.thecatapi.com", "cdn1.thecatapi.com"],
-    formats: ['image/webp'],
-    unoptimized: true
+    formats: ["image/webp"],
+    unoptimized: true,
   },
 };
 
