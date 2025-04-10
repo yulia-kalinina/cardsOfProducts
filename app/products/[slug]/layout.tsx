cb0b31bb459c22a8bade1701ae82f928f3af72ec
Yuliya Kalinina <u.v.zaika@gmail.com>
2025-04-10 11:59:12 +0400

    Setup GitHub Pages

--- app/products/[slug]/layout.tsx
+++ app/products/[slug]/layout.tsx
@@ -1,16 +1,25 @@
-import { Cat } from "@/lib/redux/catsSlice";
-
 export async function generateStaticParams() {
+  try {
+    const response = await fetch(
+      "https://api.thecatapi.com/v1/images/search?limit=20&has_breeds=1",
+      {
+        headers: {
+          "x-api-key": process.env.NEXT_PUBLIC_CAT_API_KEY || "",
+        },
+      }
+    );
 
-  const response = await fetch("https://api.thecatapi.com/v1/images/search");
-  const cats = await response.json();
+    const cats = await response.json();
 
-  const catsWithBreeds = cats.filter(
-    (cat: Cat) => cat.breeds && cat.breeds.length > 0);
+    const userCats = JSON.parse(localStorage.getItem("catsState") || "[]");
 
-  return catsWithBreeds.map((cat: { id: string }) => ({
-    slug: cat.id,
-  }));
+    return [...cats, ...userCats].map((cat: { id: string }) => ({
+      slug: cat.id,
+    }));
+  } catch (error) {
+    console.error("Error in generateStaticParams:", error);
+    return [];
+  }
 }
 
 export default function Layout({ children }: { children: React.ReactNode }) {
--- next.config.ts
+++ next.config.ts
@@ -1,16 +1,14 @@
 import type { NextConfig } from "next";
-const isExport = process.env.NODE_ENV === "production";
 
 const nextConfig: NextConfig = {
-  output: isExport ? "export" : undefined,
-  basePath: isExport ? "/cardsOfProducts" : "",
-  assetPrefix: isExport ? "/cardsOfProducts/" : "",
+  output: "export",
+  distDir: "out",
+  basePath: process.env.NODE_ENV === "production" ? "/cardsOfProducts" : "",
   images: {
     domains: ["cdn2.thecatapi.com", "cdn1.thecatapi.com"],
     formats: ["image/webp"],
     unoptimized: true,
   },
-  trailingSlash: true,
 };
 
 export default nextConfig;
