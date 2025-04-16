2cf94211b8dab6af4a116836ecb873493905ead5
Yuliya Kalinina <u.v.zaika@gmail.com>
2025-04-16 12:30:08 +0400

    fix static params

--- app/products/[slug]/page.tsx
+++ app/products/[slug]/page.tsx
@@ -5,24 +5,30 @@ import { Metadata } from "next";
 type Props = Promise<{ slug: string }>;
 
 export async function generateStaticParams() {
-  const response = await fetch(
-    "https://api.thecatapi.com/v1/images/search?limit=20&has_breeds=1",
-    {
-      headers: {
-        "x-api-key": process.env.NEXT_PUBLIC_CAT_API_KEY || "",
-      },
-    }
-  );
+  try {
+    const response = await fetch(
+      "https://api.thecatapi.com/v1/images/search?limit=20&has_breeds=1",
+      {
+        headers: {
+          "x-api-key": process.env.NEXT_PUBLIC_CAT_API_KEY || "",
+        }
+      }
+    );
 
-  const cats: Cat[] = await response.json();
-
-  const catsWithBreeds = cats.filter(
-    (cat) => cat.breeds && cat.breeds.length > 0
-  );
-
-  return catsWithBreeds.map((cat) => ({
-    slug: cat.id,
-  }));
+    if (!response.ok) throw new Error("Failed to fetch cats");
+    
+    const cats: Cat[] = await response.json();
+    
+    return cats
+      .filter(cat => cat.breeds?.length) // Фильтрация котов с breeds
+      .map(cat => ({
+        slug: cat.id
+      }));
+      
+  } catch (error) {
+    console.error("Error in generateStaticParams:", error);
+    return []; 
+  }
 }
 
 export async function generateMetadata(props: {
